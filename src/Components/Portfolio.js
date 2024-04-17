e79c0985dbf5f2f9c82c96c9c4742795accc9de4
TOBI OLADIMEJI <oluwatobilobadimeji@gmail.com>
2024-04-17 09:07:12 -0400

    Update Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,76 +1,93 @@
 import React, { Component } from 'react';
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { EffectCoverflow, Pagination, Autoplay } from 'swiper/core';
-import "swiper/swiper-bundle.min.css";
-import resumeData from './resumeData.json'; // Assuming resumeData.json is in the same directory
+// import SwiperCore, { EffectCoverflow,Pagination, Autoplay } from 'swiper/core';
+// Import Swiper React components
+// import { Swiper, SwiperSlide } from "swiper/react";
 
-SwiperCore.use([EffectCoverflow, Pagination, Autoplay]);
+// install Swiper modules
+// SwiperCore.use([EffectCoverflow,Pagination, Autoplay]);
 
 class Portfolio extends Component {
   render() {
-    if (resumeData.portfolio && resumeData.portfolio.projects) {
-      const projects = resumeData.portfolio.projects;
-      const powerBiProjects = projects
-        .filter(project => project.category === 'PowerBI')
-        .map(project => this.renderProject(project));
 
-      return (
-        <section id="portfolio">
-          <div className="row">
-            <div className="twelve columns collapsed">
-              <h1>Hey! Why Not Check Out Some of My Works.</h1>
-              <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-halves cf">
-                <div className="portfolio-category">
-                  <h2>PowerBI</h2>
-                  <Swiper
-                    effect={'coverflow'}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    slidesPerView={'auto'}
-                    coverflowEffect={{
-                      "rotate": 50,
-                      "stretch": 0,
-                      "depth": 100,
-                      "modifier": 1,
-                      "slideShadows": true,
-                      "shadow": true,
-                      "shadowOffset": 20,
-                      "shadowScale": 0.94
-                    }}
-                    autoplay={{
-                      "delay": 10000,
-                      "disableOnInteraction": false
-                    }}
-                    pagination={true}
-                    className="mySwiper">
-                    {powerBiProjects}
-                  </Swiper>
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects) {
+        var projectImage = 'images/portfolio/' + projects.image;
+        return <div key={projects.title} className="columns portfolio-item">
+          <div className="item-wrap">
+            <a target="_blank" rel="noopener noreferrer" href={projects.url} title={projects.title}>
+              <img alt={projects.title} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
                 </div>
               </div>
-            </div>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
           </div>
-        </section>
-      );
-    } else {
-      return null; // Handle the case when no data is available
+        </div>
+      })
     }
-  }
 
-  // Helper method to render individual project item
-  renderProject(project) {
-    const projectImage = 'images/portfolio/' + project.image;
+    // if(this.props.data){
+    //   var projects = this.props.data.projects.map(function(projects){
+    //     var projectImage = 'images/portfolio/'+projects.image;
+    //     return <div className="portfolio-item">
+    //       <SwiperSlide className="slide" key={projects.title} style={{backgroundImage:`url(${projectImage})`}}> 
+    //           <a target="_blank" rel="noopener noreferrer" href={projects.url} title={projects.title}>
+    //           <img src={projectImage} alt=""/>
+    //           </a>
+    //           <div className="overlay">
+    //             <div className="portfolio-item-meta">
+    //             <h5>{projects.title}</h5>
+    //                 <p>{projects.category}</p>
+    //             </div>
+    //           </div>
+    //       </SwiperSlide>
+    //       </div>
+    //   })
+    // }
     return (
-      <SwiperSlide key={project.title} className="slide" style={{ backgroundImage: `url(${projectImage})` }}>
-        <a target="_blank" rel="noopener noreferrer" href={project.url} title={project.title}>
-          <img src={projectImage} alt="" />
-          <div className="overlay">
-            <div className="portfolio-item-meta">
-              <h5>{project.title}</h5>
-              <p>{project.category}</p>
+      <section id="portfolio">
+
+        <div className="row">
+
+          <div className="twelve columns collapsed">
+
+            <h1>Hey!! Why Not Check Out Some of My Works.</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-halves cf">
+              {projects}
+            </div>
+
+            <div>
+              {/* <Swiper 
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                  "rotate": 50,
+                  "stretch": 0,
+                  "depth": 100,
+                  "modifier": 1,
+                  "slideShadows": true,
+                  "shadow": true,
+                  "shadowOffset": 20,
+                  "shadowScale": 0.94
+                }} 
+              autoplay={{
+                "delay": 10000,
+                "disableOnInteraction": false
+              }}
+              pagination={true} className="mySwiper">
+                {projects}
+              </Swiper> */}
+
             </div>
           </div>
-        </a>
-      </SwiperSlide>
+        </div>
+      </section>
     );
   }
 }
