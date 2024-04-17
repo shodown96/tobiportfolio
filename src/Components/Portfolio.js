62ab9deeab0974d19b2561aff0885d4ae1ba8f8c
TOBI OLADIMEJI <oluwatobilobadimeji@gmail.com>
2024-04-17 09:08:28 -0400

    Update Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -54,7 +54,7 @@ class Portfolio extends Component {
 
           <div className="twelve columns collapsed">
 
-            <h1>Hey!! Why Not Check Out Some of My Works.</h1>
+            <h1>Hey! Why Not Check Out Some of My Works.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-halves cf">
               {projects}
