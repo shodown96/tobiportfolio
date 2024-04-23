77a1309f701c33d27b23c62b3ccc5739b915c8e2
Elijah Soladoye <shodown96@gmail.com>
2024-04-23 16:29:53 +0100

    fix: categorization

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,62 +1,79 @@
-import React, { Component } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
-class Portfolio extends Component {
-  render() {
-    if (this.props.data) {
-      // Group projects by category
-      const projectsByCategory = {};
-      this.props.data.projects.forEach(project => {
-        if (!projectsByCategory[project.category]) {
-          projectsByCategory[project.category] = [];
-        }
-        projectsByCategory[project.category].push(project);
-      });
+function makeDistinct(array) {
+  const seen = new Set();
+  return array.filter(value => {
+    if (seen.has(value)) {
+      return false;
+    }
+    seen.add(value);
+    return true;
+  });
+}
 
-      // Generate project sections for each category
-      const projectSections = Object.keys(projectsByCategory).map(category => {
-        const categoryProjects = projectsByCategory[category].map(project => {
-          const projectImage = 'images/portfolio/' + project.image;
-          return (
-            <div key={project.title} className="columns portfolio-item">
-              <div className="item-wrap">
-                <a target="_blank" rel="noopener noreferrer" href={project.url} title={project.title}>
-                  <img alt={project.title} src={projectImage} />
-                  <div className="overlay">
-                    <div className="portfolio-item-meta">
-                      <h5>{project.title}</h5>
-                      <p>{project.description}</p>
+
+const ProjectItem = ({ project }) => (
+  <div className="columns portfolio-item" style={{ maxWidth: "500px" }}>
+    <div className="item-wrap">
+      <a target="_blank" rel="noopener noreferrer" href={project.url} title={project.title}>
+        <img alt={project.title} src={project.image} />
+        <div className="overlay">
+          <div className="portfolio-item-meta">
+            <h5>{project.title}</h5>
+            <p>{project.category}</p>
+          </div>
+        </div>
+        <div className="link-icon"><i className="fa fa-link"></i></div>
+      </a>
+    </div>
+  </div>
+)
+
+const Portfolio = ({ data }) => {
+  const [projects, setProjects] = useState([])
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    if (data) {
+      const categories = []
+      for (let project of data.projects) {
+        categories.push(project.category)
+      }
+      setCategories(makeDistinct(categories))
+      setProjects(data.projects.map(x => ({ ...x, image: `images/portfolio/${x.image}` })))
+    }
+  }, [data])
+
+
+  return (
+    <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          {/* <h1></h1> */}
+          <div id="portfolio-wrapper">
+            {categories.length > 0 ? (
+              <Fragment>
+                {categories.map(category => (
+                  <div className='portfolio-category'>
+                    <h1 style={{ color: 'black', fontSize: 20 }}>{category} Projects</h1>
+                    <div style={{
+                      display: 'flex',
+                      flexWrap: 'wrap',
+                      gap: 20,
+                      justifyItems: 'spcae-between',
+                      alignItems: 'center'
+                    }}>
+                      {projects.filter(proj => proj.category === category).map((project, i) => <ProjectItem key={i} project={project} />)}
                     </div>
                   </div>
-                  <div className="link-icon"><i className="fa fa-link"></i></div>
-                </a>
-              </div>
-            </div>
-          );
-        });
-
-        return (
-          <div key={category} className="category-section">
-            <h1 className="category-title">{category}</h1>
-            <div id={`portfolio-wrapper-${category}`} className="bgrid-thirds s-bgrid-halves cf">
-              {categoryProjects}
-            </div>
+                ))}
+              </Fragment>
+            ) : null}
           </div>
-        );
-      });
-
-      return (
-        <section id="portfolio">
-          <div className="row">
-            <div className="twelve columns collapsed">
-              {projectSections}
-            </div>
-          </div>
-        </section>
-      );
-    } else {
-      return null; // or whatever you want to render when there's no data
-    }
-  }
-}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Portfolio;
